d44457d6f94e85409f33c3c25ce4b54693d5a9f0
JIWON_MIN <mjw3534@gmail.com>
2022-06-08 14:07:33 +0900

    Create Project & Set entry, output

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// import
+const path = require('path')
+
+// export
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    clean: true
+  }
+}
+
